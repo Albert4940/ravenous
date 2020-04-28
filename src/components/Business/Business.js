4363d1a8f043d7228e41f0912c75950cf8f4cccc
Albert4940 <dorcealbertmary@gmail.com>
2020-04-27 22:33:22 -0700

    update files by passing data from App to Business and BusinessList components

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -3,39 +3,23 @@ import './Business.css';
 
 class Business extends React.Component{
 
-    
-        state = {
-            business : {
-            imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-            name: 'MarginOtto Pizzeria',
-            address: '1010 Paddington Way',
-            city: 'Flavortown',
-            state: 'NY',
-            zipCode: '10101',
-            category: 'Italian',
-            rating: 4.5,
-            reviewCount: 90
-        }
-        }
-    
-
     render(){
         return(
             <div className="Business">
             <div className="image-container">
-                <img src={this.state.business.imageSrc} alt={this.state.business.name}/>
+                <img src={this.props.business.imageSrc} alt={this.props.business.name}/>
             </div>
-            <h2>{this.state.business.name}</h2>
+            <h2>{this.props.business.name}</h2>
             <div className="Business-information">
                 <div className="Business-address">
-                <p>{this.state.business.address}</p>
-                <p>{this.state.business.city}</p>
-                <p>{this.state.business.state} {this.state.business.zipCode}</p>
+                <p>{this.props.business.address}</p>
+                <p>{this.props.business.city}</p>
+                <p>{this.props.business.props} {this.props.business.zipCode}</p>
                 </div>
                 <div className="Business-reviews">
-                <h3>{this.state.business.category}</h3>
-                <h3 className="rating">{this.state.business.rating} stars}</h3>
-                <p>{this.state.business.reviewCount} reviews</p>
+                <h3>{this.props.business.category}</h3>
+                <h3 className="rating">{this.props.business.rating} stars</h3>
+                <p>{this.props.business.reviewCount} reviews</p>
                 </div>
             </div>
             </div>
